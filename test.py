b3e4978ba95cc8c48f81e7be16f14d502890ed31
Bas van Ooyen <bas@owlin.com>
2013-10-22 16:50:01 +0200

    Initial commit

--- test.py
+++ test.py
@@ -4,9 +4,9 @@ import socket
 import sys
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect((sys.argv[1], sys.argv[2]))
+sock.connect((sys.argv[1], int(sys.argv[2])))
 
-for i in range(10):
+for i in range(int(sys.argv[3])):
     sock.sendall('set {0} testbericht \n'.format(i))
 
     sock.sendall('get {0} \n'.format(i))
